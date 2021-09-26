e83a4026c1acd5ec256c391eaa39907e8f59fb98
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-09-26 11:39:33 +0530

    Addition of urls paths

--- project/Exam_Portal/urls.py
+++ project/Exam_Portal/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("testing_frontend/", include("frontend_views_and_designs.urls"))
 ]
