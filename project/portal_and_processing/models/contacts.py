0531e57850cbe8d176a742f1e3720e1ac4bd6b94
Anik-Bardhan <anik.bardhan0406@gmail.com>
2021-10-01 20:15:13 +0530

    teacher.py file updated

--- project/portal_and_processing/models/contacts.py
+++ project/portal_and_processing/models/contacts.py
@@ -1,8 +1,13 @@
 from django.db import models
-
+from .students import Student
+from .teachers import teachers
 
 class contacts(models.Model):
     ...
     """
     Fill in the models as per DB Schema
     """
+    Name = models.ForeignKey(Student, on_delete = models.CASCADE)
+    Contact_Number = models.BigIntegerField()
+    Email = models.EmailField()
+    Teacher_name = models.ForeignKey(teachers, on_delete=models.CASCADE)
